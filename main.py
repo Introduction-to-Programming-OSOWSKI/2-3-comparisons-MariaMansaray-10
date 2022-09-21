e0f1b6ae27c3ddf01e5122456fd85de3db974514
MariaMansaray10 <833868@mystma.org>
2022-09-21 14:31:06 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-#WRITE YOUR CODE IN THIS FILE
+from operator import truediv
+
+
+def greaterThan(x,y):
+    if x>y:
+        return True
+    else: 
+        return False
+
+def lessThan(x,y):
+    if x<y:
+        return False
+    else:
+        return True
+
+def equalTo(x,y):
+    if x==y:
+        return True
+    else: 
+        return False
+
+def greaterOrEqual(x,y):
+    if x>=y:
+        return True
+    else:
+        return False
+
+def lessThan(x,y):
+    if x<=y:
+        return False
+    else:
+        return True
